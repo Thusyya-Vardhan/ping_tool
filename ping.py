75018f01a396e2e0227b94a8af1757f801c1f99c
Thusyya-Vardhan <thusyya2006@gmail.com>
2025-07-12 19:23:21 +0530

    Create ping.py

---
+++ ping.py
@@ -0,0 +1,17 @@
+import subprocess
+import ipaddress
+
+l=["ping","-n","4"] #This is for windows . 4 represents number of packets used for testing
+# use ["ping","-c","4"] for linux/mac
+var = input("enter the ip:")
+try:
+    ipaddress.ip_address(var) #used for validating ip address
+    print("Valid ip")
+except ValueError:
+    print("Invalid Ip, Please try again ")
+    exit()
+l.append(var)
+obj = subprocess.run(l,capture_output=True,text=True) 
+#capture_output = True is used to get ouput in python , else you get output in console
+#text=True makes the output into a string . output is bytes by default
+print(obj.stdout)
