81d8f7621777a8b86693a2433bb574302ce4f085
Thusyya-Vardhan <thusyya2006@gmail.com>
2025-07-13 09:51:17 +0530

    code modified

--- ping.py
+++ ping.py
@@ -1,15 +1,19 @@
 import subprocess
 import ipaddress
+import socket 
 
 l=["ping","-n","4"] #This is for windows . 4 represents number of packets used for testing
 # use ["ping","-c","4"] for linux/mac
-var = input("enter the ip:")
+var = input("enter the ip or domain:")
 try:
     ipaddress.ip_address(var) #used for validating ip address
     print("Valid ip")
 except ValueError:
-    print("Invalid Ip, Please try again ")
-    exit()
+    try:
+        var=socket.gethostbyname(var) # if the user enters domain . returns ip address
+    except socket.gaierror:
+        print("Invalid Ip, Please try again ")
+        exit()
 l.append(var)
 obj = subprocess.run(l,capture_output=True,text=True) 
 #capture_output = True is used to get ouput in python , else you get output in console
